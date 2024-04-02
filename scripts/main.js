0283e20e3fbeda1e629543c33b4283d6d7bf92bd
Uri Frazier <uri@fourkitchens.com>
2024-04-02 15:20:12 -0700

    Update princess intro img.

--- scripts/main.js
+++ scripts/main.js
@@ -21,3 +21,8 @@ $('#home-offerings-section').each(function(){
     $('#offerings-link').attr('href', '#home-offerings-section')
 });
 // Rest added via CSS: 'scroll-behavior: smooth'
+
+// Turn B&W Insight Intro Img to Color
+$('#post-intro-img').click(function(){
+    $(this).find('img').css('filter', 'none');
+})
